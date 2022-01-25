f53707dc84c3f40bfd4c77ed2e637e6c3518ce26
MINNA NIJU <86223145+minnaniju@users.noreply.github.com>
2022-01-25 13:06:21 +0530

    Update formattedsql.sql

--- formattedsql.sql
+++ formattedsql.sql
@@ -1,7 +1,6 @@
 --liquibase formatted sql
 --changeset heavy-gopher:2
-CREATE TABLE test_table
-        (startdate DATE,
-         enddate DATE,
-         class CHAR(20))
+CREATE TABLE person
+        (id INT,
+         name VARCHAR(10))
 ;
